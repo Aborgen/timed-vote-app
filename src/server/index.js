03dcec8f4459ed62be9d8d690804d5a34653cf0c
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-02-08 16:32:36 -0700

    add config folder

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,13 @@
 import makeStore   from './store';
 import reducer     from './reducer';
 import startServer from './server';
-import text     from './text.json';
+import candidates  from './config/candidates.json';
 
 export const store = makeStore(reducer);
 startServer(store, 8080);
 
 store.dispatch({
     type: 'SET_ENTRIES',
-    entries: text['entries']
+    entries: candidates['entries']
 });
 store.dispatch({type: 'NEXT'})
