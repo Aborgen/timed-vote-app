f5da2013ee161bf316abb1e7d51bcd1f93cc3492
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-02-09 17:08:52 -0700

    Minor syntatic changes. Remove placeholder data

--- src/app/App.js
+++ src/app/App.js
@@ -7,23 +7,13 @@ import ResultsView       from './Components/ResultsView/ResultsView';
 
 class App extends PureComponent {
     render() {
-        const candidates = List.of('SUPERHOT', 'Superflight', 'Valley');
-        const result = Map({
-            "winner": 'SUPERHOT',
-            "runnersUp": List.of(['Superflight', 'Valley', 'Crysis 2'])
-        });
-        const voted = 'SUPERHOT';
-        const votes = Map({'SUPERHOT': 4, 'Superflight': 3})
-
         return (
             <Switch>
                 <Route exact={true} path="/">
-                    <AppView candidates={candidates}
-                             vote={this.props.vote} />
+                    <AppView vote={this.props.vote}/>
                 </Route>
                 <Route path="/results">
-                    <ResultsView candidates={candidates}
-                                 votes={votes} />
+                    <ResultsView />
                 </Route>
             </Switch>
         );
