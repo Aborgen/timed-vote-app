56d453ba3b050a595eeadebb4917d7866164c613
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-02-12 17:19:37 -0700

    Connect frontend and backend with socket.io-client

--- src/app/index.js
+++ src/app/index.js
@@ -1,37 +1,24 @@
 import React                 from 'react';
 import ReactDOM              from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider }          from 'react-redux';
 import { createStore }       from 'redux';
 import { BrowserRouter }     from 'react-router-dom';
+import io                    from 'socket.io-client'
 import registerServiceWorker from './registerServiceWorker';
 
 import App                   from './App';
 import reducer               from './Reducers/reducer';
 
-const store = createStore(reducer);
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        ballot: {
-            candidates: ['SUPERHOT', 'Superflight', 'Valley'],
-            votes: {
-                'SUPERHOT': 3,
-                'Superflight': 3
-            }
-        }
-    }
+const store  = createStore(reducer);
+const socket = io("http://localhost:8080");
+socket.on('state', (state) =>{
+    store.dispatch({ type: 'SET_STATE', state });
 });
-const mapStateToProps = (state) => {
-    return {
-        candidates: state.getIn(['ballot', 'candidates']),
-        results: state.results
-    };
-};
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App mapState={mapStateToProps} />
+            <App />
         </BrowserRouter>
     </Provider>, document.getElementById('root')
 );
